6b985f0c60fa4be375deb22aa5dc934d261c81ed
Rehan Zafar <rehankhan983@gmail.com>
2021-12-25 15:57:44 +0530

    files added

---
+++ detection_code_image.py
@@ -0,0 +1,41 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+
+config_file = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+frozen_model = 'frozen_inference_graph.pb'
+
+model = cv2.dnn_DetectionModel(frozen_model, config_file)
+
+classLabels = []
+filename = 'labels.txt'
+with open(filename, 'rt') as spt:
+    classLabels = spt.read().rstrip('\n').split('\n')
+    
+    
+model.setInputSize(320, 320)
+model.setInputScale(1.0/127.5)
+model.setInputMean((127.5, 127.5, 127.5))
+model.setInputSwapRB(True)
+
+    
+img = cv2.imread('your image path')
+
+plt.imshow(img)     #original image
+
+classIndex, confidence, bbox = model.detect(img, confThreshold=0.55)
+
+
+font = cv2.FONT_HERSHEY_PLAIN
+
+for classInd, conf, boxes in zip(classIndex.flatten(), confidence.flatten(), bbox):
+    cv2.rectangle(img, boxes, (255, 0, 0), 2)
+    cv2.putText(img, classLabels[classInd-1], (boxes[0] + 10, boxes[1] + 40), font, fontScale = 3, color=(0, 255, 0), thickness=3)
+    
+    
+cv2.imshow('result', img)
+cv2.waitKey(0)
+
+cv2.imwrite('result.png', img)
+
---
+++ detection_code_video.py
@@ -0,0 +1,51 @@
+
+import cv2
+
+config_file = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+frozen_model = 'frozen_inference_graph.pb'
+
+model = cv2.dnn_DetectionModel(frozen_model, config_file)
+
+classLabels = []
+filename = 'labels.txt'
+with open(filename, 'rt') as spt:
+    classLabels = spt.read().rstrip('\n').split('\n')
+    
+    
+model.setInputSize(320, 320)
+model.setInputScale(1.0/127.5)
+model.setInputMean((127.5, 127.5, 127.5))
+model.setInputSwapRB(True)
+
+    
+cap = cv2.VideoCapture('test_video.mp4')
+ret, frame = cap.read()
+
+#
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+video = cv2.VideoWriter('video.avi', fourcc, 25, (frame.shape[1], frame.shape[0]))  #25 is the frame rate of output video you can change it as required
+
+
+font = cv2.FONT_HERSHEY_PLAIN
+
+while(True):
+   
+    ret, frame = cap.read()
+
+    classIndex, confidence, bbox = model.detect(frame , confThreshold=0.65)
+    if(len(classIndex) != 0):
+        for classInd, boxes in zip(classIndex.flatten(), bbox):
+            cv2.rectangle(frame, boxes, (255, 0, 0), 2)
+            cv2.putText(frame, classLabels[classInd-1], (boxes[0] + 10, boxes[1] + 40), font, fontScale = 1, color=(0, 255, 0), thickness=2)
+            
+    video.write(frame)
+    cv2.imshow('result', frame)
+    cv2.waitKey(2)
+
+
+#run these 3 lines when you close the video window OR when the video ends
+    
+cap.release()
+video.release()
+cv2.destroyAllWindows()
+
