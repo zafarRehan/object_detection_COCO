21aa53e0c32e0910b2f9d3b26ee8a3f6ebcb21a8
Rehan Zafar <rehankhan983@gmail.com>
2021-12-27 17:45:25 +0530

    Update detection_code_video.py

--- detection_code_video.py
+++ detection_code_video.py
@@ -27,23 +27,23 @@ video = cv2.VideoWriter('video.avi', fourcc, 25, (frame.shape[1], frame.shape[0]
 
 font = cv2.FONT_HERSHEY_PLAIN
 
-while(True):
-   
-    ret, frame = cap.read()
+try:
+    while(True):
 
-    classIndex, confidence, bbox = model.detect(frame , confThreshold=0.65)  #tune the confidence  as required
-    if(len(classIndex) != 0):
-        for classInd, boxes in zip(classIndex.flatten(), bbox):
-            cv2.rectangle(frame, boxes, (255, 0, 0), 2)
-            cv2.putText(frame, classLabels[classInd-1], (boxes[0] + 10, boxes[1] + 40), font, fontScale = 1, color=(0, 255, 0), thickness=2)
-            
-    video.write(frame)
-    cv2.imshow('result', frame)
-    cv2.waitKey(2)
+        ret, frame = cap.read()
 
+        classIndex, confidence, bbox = model.detect(frame , confThreshold=0.65)  #tune the confidence  as required
+        if(len(classIndex) != 0):
+            for classInd, boxes in zip(classIndex.flatten(), bbox):
+                cv2.rectangle(frame, boxes, (255, 0, 0), 2)
+                cv2.putText(frame, classLabels[classInd-1], (boxes[0] + 10, boxes[1] + 40), font, fontScale = 1, color=(0, 255, 0), thickness=2)
 
-#run these 3 lines when you close the video window OR when the video ends
-cap.release()
-video.release()
-cv2.destroyAllWindows()
+        video.write(frame)
+        cv2.imshow('result', frame)
+        cv2.waitKey(2)
+
+except:
+    cap.release()
+    video.release()
+    cv2.destroyAllWindows()
 
